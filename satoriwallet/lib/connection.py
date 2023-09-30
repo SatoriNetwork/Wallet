605a5eddd7ba02a70606c773537008bf9ff9cd24
Jordan Miller <Jordan.kay@gmail.com>
2023-09-29 22:37:31 -0600

    testing

--- satoriwallet/lib/connection.py
+++ satoriwallet/lib/connection.py
@@ -7,7 +7,6 @@ import datetime as dt
 def authPayload(wallet, challenge: str = None):
     ''' see wallet_auth in server '''
     challenge = challenge or getFullDateMessage()
-    print('AUTH PAYLOAD TIMEMSG: --- ', challenge)
     return {
         'message': challenge,
         'pubkey': wallet.publicKey,
@@ -17,6 +16,40 @@ def authPayload(wallet, challenge: str = None):
 
 def getFullDateMessage():
     ''' returns a string of today's date in UTC like this: "2022-08-01 17:28:44.748691" '''
-    x = dt.datetime.utcnow()
-    print('AUTH PAYLOAD TIME: --- ', x)
-    return str(x)
+    return str(dt.datetime.utcnow())
+
+class AuthPayload:
+    ''' 
+        {'message': '2023-09-30 04:06:32.908595', 
+        'pubkey': '021bd7999774a59b6d0e40d650c2ed24a49a54bdb0b46c922fd13afe8a4f3e4aeb', 
+        'address': 'RTEabwWn7zuTxgjwrryYtZv3ELTUidtdF7', 
+        'signature': '...'}
+    '''
+    
+    @staticmethod
+    def create(wallet, challenge: str = None) -> AuthPayload:
+        return AuthPayload(raw=authPayload(wallet, challenge))
+
+    def __init__(self, raw: dict=None):
+        self.message: str = raw.get('message')
+        self.pubkey: str = raw.get('pubkey')
+        self.address: str = raw.get('address')
+        self.signature: str = raw.get('signature')
+
+    def __str__(self):
+        return (
+            'Authenticated(
+            f'\n\tmessage: {self.message},'
+            f'\n\tpubkey: {self.pubkey},'
+            f'\n\taddress: {self.address},'
+            f'\n\tsignature: {self.signature})')
+    
+    def toJson(self):
+        return json.dumps(self.raw)
+    
+    def toDict(self):
+        return {
+            'message': self.message,
+            'pubkey': self.pubkey,
+            'address': self.address,
+            'signature': self.signature}
