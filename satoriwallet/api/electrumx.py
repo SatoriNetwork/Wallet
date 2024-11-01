5fee1592c62f5c08f5ea3f4c2b88ab658e85ab5b
Jordan Miller <Jordan.kay@gmail.com>
2024-11-01 16:47:58 -0600

    debug

--- satoriwallet/api/electrumx.py
+++ satoriwallet/api/electrumx.py
@@ -102,7 +102,9 @@ class ElectrumxAPI():
 
     # Ensure if the connection is established or not
     def _ensureConnected(self):
+        logging.debug('in _ensureConnected', color='magenta')
         if not self.connected():
+            logging.debug('in _ensureConnected', color='magenta')
             self.connect()
 
     def handshake(self) -> bool:
